32f250b8a8aed21fca38e2f4d16b253a3dd6bfad
Nicolas <nicolas_pinier@hotmail.fr>
2021-08-10 15:45:44 +0200

    Dynamics roll dice display dice Face

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ const newGame = document.getElementById('newGame');
 const rollDice = document.getElementById('rollDice');
 const hold = document.getElementById('hold');
 
+//Set Game's variables
+let currentPlayer = null;
+let diceNumber = null;
+
 //Reset game
-let resetGame = () => {
+const resetGame = () => {
   const tableSettings = [player1Score,player2Score,totalDiceRoll1,totalDiceRoll2];
   tableSettings.forEach(element => {
     element.innerHTML = 0;
@@ -23,5 +27,64 @@ let resetGame = () => {
 newGame.addEventListener('click',() => {
   alert('Nouvelle Partie !');
   resetGame();
-})
+  currentPlayer = Math.floor(Math.random()*2);
+  currentPlayerPing(currentPlayer);
+});
+
+//Small red dot indicate current player turn
+const currentPlayerPing = (number) => {
+  if(number === 0) {
+    alert('Player 1 commence !');
+
+  } else {
+    alert('Player 2 commence !');
+  }
+}
+
+//Dice Faces display
+const imgDice = document.getElementById('diceFace');
+const diceFaceUrlTable = [
+  'images/images/Dice1.png',
+  'images/images/Dice2.png',
+  'images/images/Dice3.png',
+  'images/images/Dice4.png',
+  'images/images/Dice5.png',
+  'images/images/Dice6.png'
+];
+
+//Roll Dice Display function
+const displayFace = (dicenumber) => {
+  imgDice.getAttribute("src");
+  imgDice.setAttribute("src",diceFaceUrlTable[diceNumber -1]);
+}
+
+//Roll Dice
+rollDice.addEventListener('click',() => {
+  let min = 1;
+  let max = 6;
+  diceNumber = Math.floor(Math.random()*(max - min + 1) + min);
+  displayFace(diceNumber);
+});
+
+//Change Player
+const changePlayer = () => {
+  if(currentPlayer === 1) {
+    currentPlayer = 2;
+  } else {
+    currentPlayer = 1;
+  }
+  currentPlayerPing(currentPlayer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
