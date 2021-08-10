32594064e617cef1317fd44411f65080af423261
Nicolas <nicolas_pinier@hotmail.fr>
2021-08-10 13:58:31 +0200

    Add the new/resetGame function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //Players score
-const player1Score = document.getElementById('player1Score');
-const player2Score = document.getElementById('player2Score');
+let player1Score = document.getElementById('player1Score');
+let player2Score = document.getElementById('player2Score');
 
 //Players current total number dice roll
-const totalDiceRoll1 = document.getElementById('totalDiceRoll1');
-const totalDiceRoll2 = document.getElementById('totalDiceRoll2');
+let totalDiceRoll1 = document.getElementById('totalDiceRoll1');
+let totalDiceRoll2 = document.getElementById('totalDiceRoll2');
 
 //Menu items
 const newGame = document.getElementById('newGame');
 const rollDice = document.getElementById('rollDice');
 const hold = document.getElementById('hold');
+
+//Reset game
+let resetGame = () => {
+  const tableSettings = [player1Score,player2Score,totalDiceRoll1,totalDiceRoll2];
+  tableSettings.forEach(element => {
+    element.innerHTML = 0;
+  });
+}
+
+//New Game Function
+newGame.addEventListener('click',() => {
+  alert('Nouvelle Partie !');
+  resetGame();
+})
+
