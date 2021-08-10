82f8df5c56c5649a76069eef8b80f1525d07a863
Nicolas <nicolas_pinier@hotmail.fr>
2021-08-10 13:13:04 +0200

    Select game Id elements

---
+++ script.js
@@ -0,0 +1,12 @@
+//Players score
+const player1Score = document.getElementById('player1Score');
+const player2Score = document.getElementById('player2Score');
+
+//Players current total number dice roll
+const totalDiceRoll1 = document.getElementById('totalDiceRoll1');
+const totalDiceRoll2 = document.getElementById('totalDiceRoll2');
+
+//Menu items
+const newGame = document.getElementById('newGame');
+const rollDice = document.getElementById('rollDice');
+const hold = document.getElementById('hold');
